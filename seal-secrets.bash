f8ad0d7ea29c4f847b9f9e48e98473fb6816dc12
Luca Calcaterra <calcaterra.luca@gmail.com>
2022-01-18 16:14:36 +0100

    Initial commit

---
+++ seal-secrets.bash
@@ -0,0 +1,5 @@
+kubeseal --format=yaml < ../ArgoCD-Secrets/cloudflare-api-credentials.yaml  > manifests/traefik-v2/templates/cloudflare-api-credentials-sealed.yaml
+kubeseal --format=yaml < ../ArgoCD-Secrets/traefik-sso-secrets.yaml  > manifests/traefik-forward-auth/templates/traefik-sso-secrets-sealed.yaml
+kubeseal --format=yaml < ../ArgoCD-Secrets/cloudflare-ddns-secrets.yaml  > manifests/cloudflare-ddns/cloudflare-ddns-secrets-sealed.yaml
+kubeseal --format=yaml < ../ArgoCD-Secrets/qbittorrent-vpn-config-secrets.yaml  > manifests/qbittorrent/templates/qbittorrent-vpn-config-secrets.yaml
+kubeseal --format=yaml < ../ArgoCD-Secrets/qbittorrent-vpn-auth-secrets.yaml  > manifests/qbittorrent/templates/qbittorrent-vpn-auth-secrets.yaml
